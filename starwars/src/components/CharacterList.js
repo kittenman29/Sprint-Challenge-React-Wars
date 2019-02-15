eb2700a6ab0495d4d2ae32d0dfa9712aca04a46a
Will Schulz <will.schulz29@gmail.com>
2019-02-15 14:47:16 -0400

    question about styling

--- starwars/src/components/CharacterList.js
+++ starwars/src/components/CharacterList.js
@@ -3,10 +3,6 @@ import Characters from './Characters'
 import './StarWars.css'
 
 class CharacterList extends Component {
-    constructor() {
-        super();
-    }
-
     render() {
         return this.props.starwarsChars.map((starwarsChars, index) => (
             <Characters key={index} starwarsChars={starwarsChars} />
--- starwars/src/components/Characters.js
+++ starwars/src/components/Characters.js
@@ -2,9 +2,21 @@ import React, { Component } from 'react';
 
 
 class Characters extends Component {
-    constructor() {
-        super();
+
+    getStyle = () => {
+        if(this.props.starwarsChars.gender === 'male' || 'female') {
+            return {
+                color: 'purple'
+            } 
+        }   else {
+            console.log(this.props.starwarsChars.gender);
+            return {
+                
+                display: 'none'
+            }
+        }
     }
+
 
     render() {
         return (
@@ -12,6 +24,7 @@ class Characters extends Component {
                 <h1 className='characterName'>Name: { this.props.starwarsChars.name }</h1>
                 <p className='characterYear'>Birth Year: { this.props.starwarsChars.birth_year }</p>
                 <p className='characterHeight'>Height: { this.props.starwarsChars.height }</p>
+                <p style={this.getStyle()} className='characterGender'>Gender: { this.props.starwarsChars.gender }</p>
             </div>
         );
     }
